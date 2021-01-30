b5bd9d8d604b488c00b1904dc83eb1739a89232e
wolfpat01 <haithem2001@gmail.com>
2021-01-30 15:20:15 +0100

    stanks

---
+++ foreachsync.js
@@ -0,0 +1,37 @@
+let array = [
+    "first", "second", "third"
+];
+
+// await for a specific time then resolves
+const uwait = (e) => {
+    return new Promise((s) => setTimeout(() => s(), e));
+}
+
+// simulate random write speeds
+const srw = async (data) => {
+    await uwait(Math.random() * 100)
+    console.log(data)
+}
+
+// the function
+Array.prototype.forEach = async function (cb) {
+    if (cb.constructor.name === "AsyncFunction") {
+        for (let i = 0; i < this.length; i++) {
+            await cb(this[i], i)
+        }
+    } else {
+        for (let i = 0; i < this.length; i++) {
+            cb(this[i], i)
+        }
+    }
+};
+
+// async arrow function being called
+(async () => {
+    array.forEachSync((e, index) => {
+        srw(`${index}->${e}`)
+    })
+
+})()
+
+
